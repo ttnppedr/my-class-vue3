5b6632c7c8c2cc4ec9c2049528cea58bc82c0869
ttnppedr <priest0204@gmail.com>
2022-07-03 18:39:52 +0800

    validate email

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -4,7 +4,10 @@
       <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
         <MailIcon class="h-8 w-6 text-gray-500" aria-hidden="true" />
       </div>
-      <input type="email" name="email" id="email" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 text-3xl border-gray-300 rounded-md placeholder:text-2xl placeholder:text-gray-400" placeholder="you@example.com" @keyup.enter="login"/>
+      <input type="email" name="email" id="email" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 text-3xl border-gray-300 rounded-md placeholder:text-2xl placeholder:text-gray-400" placeholder="you@example.com" @keyup.enter="login" @input="resetMessage"/>
+    </div>
+    <div class="mt-3 text-xl">
+      <span class="text-red-500">{{ errorMessage }}</span>
     </div>
     <div class="mt-3 text-xl">
       <span v-if="isLoading">登入中...</span>
@@ -19,9 +22,13 @@ import { MailIcon } from '@heroicons/vue/solid'
 import axios from 'redaxios';
 import { useMutation } from "vue-query";
 import { useAuthStore } from '@/stores/auth'
+import * as yup from 'yup';
+import { ref } from 'vue';
 
 const apiEndPoint = import.meta.env.VITE_API_ENDPOINT;
 const auth = useAuthStore()
+
+let errorMessage = ref('')
 
 function loginMutation() {
   return useMutation((email) => axios.post(`${apiEndPoint}/login`, JSON.stringify(email)), {
@@ -35,7 +42,16 @@ function loginMutation() {
 
 const { isLoading, isError, error, isSuccess, mutate } = loginMutation();
 
-function login(e) {
-  mutate({email: e.target.value});
+async function login(e) {
+  try {
+    await yup.string().email('Email 格式不正確').required('請輸入 Email').validate(e.target.value)
+    mutate({email: e.target.value});
+  } catch (e) {
+    errorMessage.value = e.errors[0]
+  }
+}
+
+function resetMessage() {
+  errorMessage.value = ''
 }
 </script>
